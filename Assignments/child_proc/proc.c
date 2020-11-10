d4f221b2f93ffbc59fa276faf19e2d1daeb687cf
shashwath <shaswathts@gmail.com>
2020-11-10 19:08:14 +0530

    Creating 3 child process

--- Assignments/child_proc/proc.c
+++ Assignments/child_proc/proc.c
@@ -1,21 +1,48 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <sys/types.h>
+#include <stdlib.h>
 
-int main (void) {
-    int ret_value, count = 3;
+int main(){
+    
+    printf("I am Parrent process my PPID is: %d\n", getppid());
+    printf("My process PID is: %d\n",getpid());
+    printf("creating a child process\n"); 
+    
+    int ret_value1, ret_value2, ret_value3;
     ret_value1 = fork();
-    for (int i = 1; i <= count; i++) {
+    
+    if(ret_value1 < 0) {
+        printf("\nchild process not created so quitting...\n");
+        exit(1);
+    }
+    
+    else if(ret_value1 == 0) {
+        printf("\nInside the child process 1\n");
+        printf("The child id process PID: %d\n",getpid());
+        printf("The parent PPID is %d & child process is %d\n",getppid(), getpid());
+        //sleep(3);
+        ret_value2 = fork();
         
-        if (ret_value > 0) {
-            printf("I am parrent & My pid is %d\n",  getpid());
+        if(ret_value2 == 0) {
+            printf("\nInside the child process 2\n");
+            printf("The child id process PID: %d\n",getpid());
+            printf("The parent PPID is %d & child process is %d\n",getppid(), getpid());
+            //sleep(4);
+            ret_value3 = fork();
+            
+            if (ret_value3 == 0) {
+                printf("\nInside the child process 3\n");
+                printf("The child's process PID is: %d\n",getpid());
+                printf("The parent PPID is %d & child process is %d\n",getppid(), getpid());
+                //sleep(5);
+                exit(1);    
+            }
         }
-        else (ret_value == 0) {
-            sleep(0.5);
-            printf("I am child & My pid is %d\n", getpid());
-            printf("My parent's pid is %d\n", getppid());
-        }
-        continue;
-    } 
-    return 0;
+        
+    }
+    
+    else {
+        sleep(5); 
+        printf("\nNow back in Parrent process PPID=%d & current process PID=%d\n\n", getppid(), getpid());    
+    }
 }
