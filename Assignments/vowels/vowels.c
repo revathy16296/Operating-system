9ba5a6d1263391725b6c94a680ec94f786234802
shashwath <shaswathts@gmail.com>
2020-11-11 15:55:48 +0530

    Pipe connection not working

--- Assignments/vowels/vowels.c
+++ Assignments/vowels/vowels.c
@@ -8,18 +8,21 @@ parent process.*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include<sys/wait.h>
+#include <sys/wait.h>
+#include <time.h>
 #include <string.h>
-
 
 int main(int argc, char * argv[]) {
     
-    printf("In Parent PID: %d\n", getpid());
-    char * vowel[10] = {"a","e","i","o","u"};
-    char string[30];
-    strcpy(string, argv[1]);
-    int strLen, countOfVowel = 0;
+    printf("\nIn Parent (PID=%d)\n", getpid());
+    //fd[0] = read from 
+    //fd[1] = write from
+    int fd[2];
+    if (pipe(fd) == -1 ) {
+        printf("\nPipe not created..\n");
+        return EXIT_FAILURE;
+    }
+
     int ret_value = fork();
     //printf("string: %s\n", string);
 
@@ -30,24 +33,49 @@ int main(int argc, char * argv[]) {
     
     else if(ret_value == 0) {
         
-        printf("In child PID: %d\n", getpid());
+        close(fd[0]);
+        int strLen;
+        char * vowel[10] = {"a","e","i","o","u"};
+        char string[30];
+        strcpy(string, argv[1]);
+        printf("In child (PID=%d)\n", getpid());
         strLen = strlen(string);
         
-        for(int i = 0; i <= strLen; i++) {
+        int j;
+        for(int i = 0; i < strLen; i++) {
             //if vowel exists in string then add + 1 to countOfVowel 
             if (strchr(string, *vowel[i]) != NULL) {
-                printf("string: %s\n", string);
-                countOfVowel++; 	
+                //printf("string: %s\n", string);
+                j++;
+                printf("(IN IF LOOP)Total count of vowel in string %d\n", j); 	
             }
-            printf("Total count of vowel in string %d\n", countOfVowel);
+            printf("(IN FOR LOOP)Total count of vowel in string %d\n", j);    
         }
+        //return j;
+        if (write(fd[1], &j, sizeof(int)) == -1) {
+                printf("not able to write..\n");
+                return 1;
+            }
+            sleep(1);
+            close(fd[1]);
+        printf("(IN ELSE IF LOOP)Total count of vowel in string %d\n", j);
+              
     }
     
     else {
         
         wait(NULL);
+        close(fd[1]); 
+        int op_of_child;
+        if (read(fd[0], &op_of_child, sizeof(int)) == -1) {
+            printf("not able to write..\n");
+            return 1;
+        }
+        close(fd[0]);
         //read(&countOfVowel);
-        printf("Total count of vowel in string %d\n", countOfVowel);
-        printf("Back in Parrent process PID: %d\n", getpid());    
+        //printf("Total count of vowel in string %d\n", countOfVowel);
+        printf("\nBack in Parrent process (PID=%d)\n", getpid());
+        printf("Total count of vowel in string %d\n\n", op_of_child);    
     }
+    return EXIT_SUCCESS;
 }
