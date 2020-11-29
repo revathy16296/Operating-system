6b0a2f9b0964bbdca9c8bf0752afb15a4cdbaa48
shashwath <shaswathts@gmail.com>
2020-11-29 17:27:17 +0530

    Swaping two numbers using structs & threads

--- threads/01_swapNum.c
+++ threads/01_swapNum.c
@@ -1,30 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-// #define Message 85
 
-void *entry_Point(void *value1, void *value2) {
+struct swaping_numbers {
+    int num1;
+    int num2;
+}; typedef struct swaping_numbers swap;
+
+void *entry_Point(void *value) {
     
     printf("\nSwapping inside thread 1 :XD \n");
     
-    /*int temp = value1;
-    value1 = value2;
-    value1 = temp; */
-
-    return NULL;
+    swap temp;
+    printf("Before swap: num1 = %d and num2 = %d\n", temp.num1, temp.num2); 
+    temp.num1 = &value[1];
+    temp.num2 = &value[0];
+    printf("After swap: num1 = %d and num2 = %d\n", temp.num1, temp.num2); 
 }
 
 int main(int argc, char *argv[]) {
     
-    int num1 = 123, num2 = 321;
+    swap nNum;
+    nNum.num1 = 123;
+    nNum.num2 = 321;
     printf("Hello from the main thread :D \n");
-    printf("Before swap: num1 = %d and num2 = %d\n", num1, num2);
+    printf("Before swap: num1 = %d and num2 = %d\n", nNum.num1, nNum.num2);
 
     pthread_t thread1;
-    pthread_create(&thread1, NULL, entry_Point, (&num1, &num2));
+    pthread_create(&thread1, NULL, entry_Point, &nNum);
     pthread_join(thread1, NULL);
 
-    printf("After swap: num1 = %d and num2 = %d\n", num1, num2);
+    printf("After swap: num1 = %d and num2 = %d\n", nNum.num1, nNum.num2);
     
     return EXIT_SUCCESS;
 }
