a59e965369404512f4937e2a770924dcaff91206
shashwath <shaswathts@gmail.com>
2020-12-14 21:12:56 +0530

    Array basics

---
+++ lab/03_array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int addEvnOdd(int one[])
+{
+	for (int i = 0; i < 10; i++) {
+	
+	}
+}
+
+int main () 
+{
+	int first[] = {1,2,3,4,5,6,7,8,9,10};
+	
+	addEvnOdd(first);
+
+	return EXIT_SUCCESS;
+}
