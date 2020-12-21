1066535ac2ab4bf8271a37a9bad58bfe39111d40
shashwath <shaswathts@gmail.com>
2020-12-21 15:50:33 +0530

    RTOS lab test 1st question

---
+++ lab/04_labTest1.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int injectArray(int a[], int b[], int size1, int size2)
+{
+	printf("\nInjecting b[] at (m/2)th index of a[]\n\n");
+	int buffer[15];
+	int s = size1/2;
+	
+	for (int i = 0; i < 15; i++) {
+		if ((i < s) || (i > s+size2-1)) {
+			buffer[i] = *a;
+			*a++;
+		} 
+		else if ((i > s-1) && (i < (s+size2+1))){
+			buffer[i] = *b;
+			*b++;
+		}
+		printf("buffer[%d] = %d\n", i, buffer[i]);
+	}	
+}
+
+int replaceArray(int a[], int b[])
+{
+	printf("Replacing Even index of b[] with odd index of a[]\n\n");
+	for(int i = 0; i < 5; i++) {
+		if ((i % 2) == 0) { 
+			b[i] = a[i + 1];
+		}
+		printf("b[%d] = %d\n", i, b[i]);
+	}
+}
+
+int main ()
+{
+	int a[] = {1,2,3,4,5,6,7,8,9,10};
+	int b[] = {11,12,13,14,15};
+
+	int size1 = sizeof(a)/sizeof(a[0]);
+	int size2 = sizeof(b)/sizeof(b[0]);
+
+	replaceArray(&a, &b);
+	injectArray(&a, &b, size1, size2);
+}
