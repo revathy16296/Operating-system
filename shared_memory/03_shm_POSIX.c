a13723586cc420a90aff8e17db73b93a5138b783
shashwath <shaswathts@gmail.com>
2021-02-09 10:10:23 +0530

    Basics of shared memory

--- shared_memory/03_shm_POSIX.c
+++ shared_memory/03_shm_POSIX.c
@@ -5,74 +5,25 @@
 #include <sys/shm.h> 
 #include <sys/stat.h> 
 
-#define MAX_L 10
-
-const int SIZE = 4096; 
-int prod(int array2[])
+int main () 
 {
-    int prod = 1;
-    for(int i = 0; i < 10; i++) {
-        prod = prod * array2[i];
-    }
-    return prod;
-}
+    const int SIZE = 4096;
+    const char *name = "OS";
+    const char *msg_0 = "Hello";
+    const char *msg_1 = "World";
 
-int sum(int array2[])
-{
-    int sum = 0;
-    for(int i = 0; i < 10; i++) {
-        sum = sum + array2[i];
-    }
-    return sum;
-}
+    int shm_fd;
+    void *ptr;
+    
+    shm_fd = shm_open(name, O_CREAT | O_RDWR, 0666);
+    ftruncate(shm_fd, SIZE);
 
-int main()
-{
-    int    ShmID; // variable for stroing SHm id
-    const char *name = "OS";  // pointer for shared memory
-    pid_t  pid;
-    int *ptr; 
-    int array1[MAX_L];
-    printf("entering the file ");
-    printf("enter the elements of the array with 10 elements only ");
-    for(int i = 0; i < 10 ; i++)   {
-        scanf("%d",&array1[i]);
-    }
+    ptr = mmap(0, SIZE, PROT_WRITE, MAP_SHARED, shm_fd, 0);
 
-    ShmID = shm_open(name, O_CREAT | O_RDWR, 0666);  //specify the permission...
-     if (ShmID < 0) {
-        printf("* shmget error (server) *\n");
-        exit(1);
-     }
+    sprintf(ptr, "%s", msg_0);
+    ptr += strlen(msg_0);
+    sprintf(ptr, "%s", msg_1);
+    ptr += strlen(msg_0);
 
-  // printf("Server has received a shared memory of four integers...\n");
-
-    /// ShmPTR = (int *) shmat(ShmID, NULL, 0);
-	ftruncate(ShmID, SIZE); 
-	ptr = mmap(0, SIZE, PROT_WRITE , MAP_SHARED, ShmID, 0); 
-    pid = fork();
-    if(pid == 0) {
-        int sum1, prod1;
-        sum1 = sum(array1);
-        prod1 = prod(array1);
-
-        printf("the product of the elements is %d\n",prod1);
-        printf("the sum of the elements %d\n",sum1);
-        ptr[0] = sum1;
-        printf("shared the sum value in the shared memory\n");
-    }
-    else {
-        wait();
-        int sum2;
-        float avg;
-        sum2 = ptr[0];
-        printf("accessing the shared memory\n");
-        //printf("the sum received from shm is %d",ShmPTR[0]);
-        avg = sum2/10;
-        printf("the avg of the elements is %f\n",avg);
-                 
-        printf("the parent process has detached the shared memory\n ");
-        shm_unlink(name);
-        printf("the parent process has removed the shared memory...\n");
-    }
+    return EXIT_SUCCESS;
 }
